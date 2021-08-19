9dffdf6a103e3295d0fc75f724fe5d79c18b0e47
Michael Pabon <michael@mac-mini.lan>
2021-08-18 23:56:25 -0400

    Inital Commit

---
+++ src/main/java/gg/wzleagues/wzleagues/WzleaguesApplication.java
@@ -0,0 +1,13 @@
+package gg.wzleagues.wzleagues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzleaguesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzleaguesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gg/wzleagues/wzleagues/WzleaguesApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.wzleagues.wzleagues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzleaguesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
